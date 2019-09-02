8e88d62a3b3345559dd370931b20f6851fbaa8de
Andrew <Andrew@DESKTOP-ORURSSA.nc.rr.com>
2019-09-02 01:03:51 -0400

    test1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -5,10 +5,79 @@ import java.util.Scanner;
 public class A1Adept {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		adept(scan);
 	}
+
+	public static void adept(Scanner scan) {
+		String maxName="";
+		String minName="";
+		int storeItems = scan.nextInt();
+		String[] names = new String[storeItems];
+		Double[] prices = new Double[storeItems];
+		for (int i = 0; i < storeItems; i++) {
+			names[i] = scan.next();
+			prices[i] = scan.nextDouble();
+			// possibly prices[storeItems]
+		}
+		int numPeople = scan.nextInt();
+		Double[] total = new Double[numPeople];
+		String[] firstName = new String[numPeople];
+		String[] lastName = new String[numPeople];
+		for (int j = 0; j < numPeople; j++) {
+			firstName[j] = scan.next();
+			lastName[j] = scan.next();
+			int numItems = scan.nextInt();
+			String[] items = new String[numItems];
+			int[] itemNumber = new int[numItems];
+			Double cost = 0.0;
+			for (int k = 0; k < numItems; k++) {
+				itemNumber[k] = scan.nextInt();
+				items[k] = scan.next();
+				for (int l = 0; l < storeItems; l++) {
+					if (items[k].equals(names[l])) {
+						cost += prices[l] * itemNumber[k];
+						// System.out.println(lastName[j] + " " + items[k] + " " + cost) ;
+
+					}
+
+				}
+
+			}
+			total[j] = cost;
+			// System.out.println(cost);
+		}
+		
+		Double max = total[0];
+		for (int m = 0; m < total.length; m++) {
+			if (total[m] >= max) {
+				max = total[m];
+				maxName = firstName[m] + " " + lastName[m];
+				// System.out.println(maxName);
+			}
+
+		}
+		Double min = total[0];
+		for (int n = 0; n < total.length; n++) {
+			if (total[n] <= min) {
+				min = total[n];
+				minName = firstName[n] + " " + lastName[n];
+				//System.out.println(minName);
+
+			}
+
+		}
+		Double avgTotal = 0.0;
+		for (int o = 0; o < total.length; o++) {
+			avgTotal += total[o];
+		}
+		Double avg = (avgTotal / total.length);
+
+		System.out.println("Biggest: "+maxName +" ("+String.format("%.2f", max)+")");
+		System.out.println("Smallest: "+minName +" ("+String.format("%.2f", min)+")");
+		System.out.println("Average: "+String.format("%.2f", avg));
+	}
+	
+
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,33 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		// Your code goes here.
+		process(scan);
 	}
+
+	public static void process(Scanner scan) {
+		int numCustomers = scan.nextInt();
+		double total =0.0;
+		for (int i = 0; i < numCustomers; i++) {
+			String firstName = scan.next();
+			char firstInitial = firstName.charAt(0);
+			String lastName = scan.next();
+			int numItems = scan.nextInt();
+			for (int j = 0; j < numItems; j++) {
+				int numThing = scan.nextInt();
+				String itemName = scan.next();
+				double price = scan.nextDouble();
+				double cost = price*numThing;
+				//System.out.println(cost);
+			
+				total += cost;
+				
+			}
+			System.out.println(firstInitial+"."+ " "+ lastName+ ": "+ String.format("%.2f", total));
+			total = 0 ;
+		}
+	}
+	// You can define / use static helper methods here.
 }
